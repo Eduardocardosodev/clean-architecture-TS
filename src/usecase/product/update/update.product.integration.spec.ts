0790a132c9dd3088731c414b642bb80887eed08e
Eduardocardosodev <eduardocardoso.dev@gmail.com>
2024-06-12 17:04:21 -0300

    fix: :bug: Fixing bug in tests

--- src/usecase/product/update/update.product.integration.spec.ts
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 import ProductModel from '../../../infrastructure/product/repository/sequelize/product.model';
 import ProductRepository from '../../../infrastructure/product/repository/sequelize/product.repository';
 import UpdateProductUseCase from './update.product.usecase';
+import Product from '../../../domain/product/entity/product';
 
 describe('Test update product use case', () => {
   let sequelize: Sequelize;
@@ -24,7 +25,10 @@ describe('Test update product use case', () => {
 
   it('should update a product', async () => {
     const productRepository = new ProductRepository();
+    const createProduct = new Product('123', 'Banana', 30.0);
     const useCase = new UpdateProductUseCase(productRepository);
+
+    await productRepository.create(createProduct);
 
     const updatedProductInput = {
       id: '123',
