b30d56c78049c3835d3205ae042d203e6fe46f5e
Eduardocardosodev <eduardocardoso.dev@gmail.com>
2024-06-17 23:09:15 -0300

    feat: :sparkles: add a new test in product

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -13,6 +13,12 @@ describe('Product unit tests', () => {
     }).toThrowError('product: Name is required');
   });
 
+  it('should throw error when name and id are empty', () => {
+    expect(() => {
+      const product = new Product('', '', 10);
+    }).toThrowError('product: Id is required,product: Name is required');
+  });
+
   it('should throw error when price is less than zero', () => {
     expect(() => {
       const product = new Product('123', 'Name', -1);
